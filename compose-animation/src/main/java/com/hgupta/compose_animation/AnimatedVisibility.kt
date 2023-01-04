0e22493c943ad300292406129d03d3d1b2008454
hritikpgupta <hritikgupta722@gmail.com>
2023-01-04 14:30:53 +0000

    Completed Animation and Gesture

--- compose-animation/src/main/java/com/hgupta/compose_animation/AnimatedVisibility.kt
+++ compose-animation/src/main/java/com/hgupta/compose_animation/AnimatedVisibility.kt
@@ -3,15 +3,21 @@ package com.hgupta.compose_animation
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntSize
@@ -20,7 +26,6 @@ import androidx.compose.ui.unit.dp
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AnimatedVisibilitySample() {
-
 
     Column(modifier = Modifier.fillMaxSize()) {
         var visible by remember { mutableStateOf(false) }
@@ -50,9 +55,13 @@ fun AnimatedVisibilitySample() {
                 repeatMode = RepeatMode.Reverse, animation = tween(2000)
             )
         )
+        PressIconButton(onClick = {},
+            icon = { Icon(Icons.Filled.ShoppingCart, contentDescription = null) },
+            text = { Text("Add to cart") })
+        SwipeableSample()
         Box(
             modifier = Modifier
-                .size(200.dp)
+                .size(50.dp)
                 .clip(RoundedCornerShape(borderRadius))
                 .background(color)
         ) {
@@ -70,7 +79,7 @@ fun AnimatedVisibilitySample() {
 //            )
 //        }
         AnimatedContent(targetState = visible,
-            modifier = Modifier.size(width = 100.dp, height = 100.dp),
+            modifier = Modifier.size(width = 40.dp, height = 40.dp),
             content = { isVisible ->
                 if (isRound) {
                     Box(modifier = Modifier.background(Color.Green))
@@ -107,18 +116,74 @@ fun AnimatedVisibilitySample() {
             if (targetExpanded) {
                 Box(
                     modifier = Modifier
-                        .size(width = 400.dp, height = 250.dp)
+                        .size(width = 150.dp, height = 100.dp)
                         .background(color = Color.Cyan.copy(0.4f)),
                 )
             } else {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "",
-                    modifier = Modifier.size(width = 50.dp, height = 50.dp)
+                Box(
+                    modifier = Modifier
+                        .size(width = 80.dp, height = 80.dp)
+                        .background(color = Color.Cyan.copy(0.4f)),
                 )
             }
         }
+        TransformableSample()
+
     }
 
 
+}
+
+@Composable
+fun TransformableSample() {
+    // set up all transformation states
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
+    Box(
+        Modifier
+            // apply other transformations like rotation and zoom
+            // on the pizza slice emoji
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                rotationZ = rotation,
+                translationX = offset.x,
+                translationY = offset.y
+            )
+            // add transformable to listen to multitouch transformation events
+            // after offset
+            .transformable(state = state)
+            .background(Color.Blue)
+            .size(width = 150.dp, height = 150.dp)
+    )
+}
+
+@Composable
+fun PressIconButton(
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    text: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    val isPressed by interactionSource.collectIsPressedAsState()
+    Button(
+        onClick = onClick, modifier = modifier, interactionSource = interactionSource
+    ) {
+        AnimatedVisibility(visible = isPressed) {
+            if (isPressed) {
+                Row {
+                    icon()
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                }
+            }
+        }
+        text()
+    }
 }
--- compose-animation/src/main/java/com/hgupta/compose_animation/MainActivity.kt
+++ compose-animation/src/main/java/com/hgupta/compose_animation/MainActivity.kt
@@ -3,28 +3,116 @@ package com.hgupta.compose_animation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.hgupta.compose_animation.ui.theme.ComposeSamplesTheme
+import kotlin.math.roundToInt
+import androidx.compose.ui.graphics.Color as cc
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeSamplesTheme {
                 // A surface container using the 'background' color from the theme
+                var selected by remember { mutableStateOf(false) }
+                // Animates changes when `selected` is changed.
+                val transition = updateTransition(selected, label = "")
+                val borderColor by transition.animateColor(label = "") { isSelected ->
+                    if (isSelected) cc(2247)
+                    else cc(2247)
+                }
+                val elevation by transition.animateDp(label = "") { isSelected ->
+                    if (isSelected) 10.dp else 2.dp
+                }
+                val color = remember { Animatable(cc.Yellow) }
+                var offsetXx by remember { mutableStateOf(0f) }
+
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    onClick = { selected = !selected },
+                    shape = RoundedCornerShape(8.dp),
+                    border = BorderStroke(2.dp, borderColor),
+                    elevation = elevation
                 ) {
-                    MyApp()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .scrollable(
+                                orientation = Orientation.Vertical,
+                                state = rememberScrollState()
+                            )
+                            .padding(16.dp)
+                    ) {
+                        Text(text = "Hello, world!")
+                        // AnimatedVisibility as a part of the transition.
+                        transition.AnimatedVisibility(
+                            visible = { targetSelected -> targetSelected },
+                            enter = expandVertically(),
+                            exit = shrinkVertically()
+                        ) {
+                            Text(text = "It is fine today.")
+                        }
+                        // AnimatedContent as a part of the transition.
+                        transition.AnimatedContent { targetState ->
+                            if (targetState) {
+                                Text(text = "Selected")
+                            } else {
+                                Icon(
+                                    imageVector = Icons.Default.Phone, contentDescription = "Phone"
+                                )
+                            }
+                        }
+                        Text(
+                            modifier = Modifier
+                                .offset { IntOffset(offsetXx.roundToInt(), 0) }
+                                .draggable(orientation = Orientation.Horizontal,
+                                    state = rememberDraggableState { delta ->
+                                        offsetXx += delta
+                                    }),
+                            text = "Drag me!",
+                        )
+                        Box() {
+                            var offsetX by remember { mutableStateOf(0f) }
+                            var offsetY by remember { mutableStateOf(0f) }
+                            Box(
+                                Modifier
+                                    .offset {
+                                        IntOffset(
+                                            offsetX.roundToInt(), offsetY.roundToInt()
+                                        )
+                                    }
+                                    .background(cc.Blue)
+                                    .size(50.dp)
+                                    .pointerInput(Unit) {
+                                        detectDragGestures { change, dragAmount ->
+                                            change.consumeAllChanges()
+                                            offsetX += dragAmount.x
+                                            offsetY += dragAmount.y
+                                        }
+                                    })
+                        }
+                        MyApp()
+                    }
                 }
             }
         }
@@ -34,6 +122,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     AnimatedVisibilitySample()
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeableSample() {
+    val width = 96.dp
+    val squareSize = 48.dp
+    val swipeableState = rememberSwipeableState(0)
+    val sizePx = with(LocalDensity.current) { squareSize.toPx() }
+    val anchors = mapOf(0f to 0, sizePx to 1) // Maps anchor points (in px) to states
+
+    Box(
+        modifier = Modifier
+            .width(width)
+            .swipeable(
+                state = swipeableState,
+                anchors = anchors,
+                thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                orientation = Orientation.Horizontal
+            )
+            .background(cc.LightGray)
+    ) {
+        Box(
+            Modifier
+                .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
+                .size(squareSize)
+                .background(cc.DarkGray)
+        )
+    }
 }
 
 @Preview(showBackground = true)
